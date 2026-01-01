0b1bc6d2a57afd9b7a13f8985b130400864604f1
hybrideology <30223844+hybrideology@users.noreply.github.com>
2026-01-01 10:52:14 -0600

    drop vpn for tailscale

--- nix/hosts/ceres/configuration.nix
+++ nix/hosts/ceres/configuration.nix
@@ -4,10 +4,11 @@
     ./disko.nix
     ./hardware.nix
     ./users.nix
-    ./vpn.nix
+    # ./vpn.nix
   ];
   time.timeZone = "America/Chicago";
   networking.hostName = "ceres";
   networking.hostId = "e0fdcfa7"; # random, required by zfs
   system.stateVersion = "25.11";
+  services.tailscale.enable = true;
 }
