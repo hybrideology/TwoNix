b1effd87aeea258649fdda737e0a9b1327916b8a
hybrideology <30223844+hybrideology@users.noreply.github.com>
2026-02-19 22:51:49 -0600

    fix typo

--- nix/hosts/ceres/matrix-server.nix
+++ nix/hosts/ceres/matrix-server.nix
@@ -114,7 +114,7 @@ in {
           };
           permissions = {
             "*" = "relay";
-            "${config.services.matrix-tuwunel.settings.globlal.server_name}" = "user";
+            "${config.services.matrix-tuwunel.settings.global.server_name}" = "user";
             "@pqvqn:matrix.org" = "user";
             "@hybrideology:${config.services.matrix-tuwunel.settings.global.server_name}" = "admin";
           };
