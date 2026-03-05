6bf53cd41108ba7009e1a8f21113f5745e4a84f5
hybrideology <30223844+hybrideology@users.noreply.github.com>
2026-03-04 20:14:43 -0600

    swap wine package

--- nix/modules/home/lutris.nix
+++ nix/modules/home/lutris.nix
@@ -7,7 +7,7 @@
     enable = true;
     protonPackages = [pkgs.proton-ge-bin];
     steamPackage = osConfig.programs.steam.package;
-    winePackages = [pkgs.wineWow64Packages.full];
+    winePackages = [pkgs.wineWowPackages.waylandFull];
   };
   vars.persistence.dirs = [".local/share/lutris"];
 }
